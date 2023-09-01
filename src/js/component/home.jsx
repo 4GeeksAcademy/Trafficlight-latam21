1ce0b0aef08dbcc6d84ec697cacae78d1302d415
Frossting <109557017+Frossting@users.noreply.github.com>
2023-09-01 02:15:32 +0000

    lighs-color

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import "../../styles/index.css";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [activeLigh, setActiveLigh] = useState("");
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="support"></div>
+			<div className="box">
+				<div>
+					<button 
+						className= {activeLigh == "red" ? "circule lighcolorred shadowred" : "circule lighcolorred"} 
+						onClick={() =>{
+							setActiveLigh("red")
+							console.log("red" , activeLigh)
+						}}>
+					</button>
+				</div>
+				<div>
+					<button 
+						className= {activeLigh == "orange" ? "circule lighcolororange shadoworange" : "circule lighcolororange"} 
+						onClick={() =>{
+							setActiveLigh("orange")
+							console.log("orange" , activeLigh)
+						}}>
+					</button>
+				</div>
+				<div>
+				   	<button 
+						className= {activeLigh == "green" ? "circule lighcolorgreen shadowgreen" : "circule lighcolorgreen"} 
+						onClick={() =>{
+							setActiveLigh("green")
+							console.log("green" , activeLigh)
+						}}>	
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
